1285fd7278266b2d715bce1c91e2541e4deee849
K Linh <levuklinh@gmail.com>
2019-08-19 18:34:35 +0800

    New look

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-
 $(document).ready(function() {
     
     $('.menu').on('click', function() {
         $('html').toggleClass('menu-open');
     });
-    
+
+    var sections = $('section')
+  , nav = $('.nav__bar')
+  , nav_height = nav.outerHeight();
+ 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+ 
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top && cur_pos <= bottom) {
+      $('.tabLink').removeClass('active');
+      sections.removeClass('active');
+ 
+      $(this).addClass('active');
+      $('.nav__bar').find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+         
     
 });
+
 
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
